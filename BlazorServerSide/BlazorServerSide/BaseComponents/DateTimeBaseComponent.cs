f481eb9ab32f9f4abc052a2f75751166cbc6c98f
vivekreddy13 <vivekreddy2999@gmail.com>
2021-02-04 12:19:27 +0530

    My frst commit

---
+++ BlazorServerSide/BlazorServerSide/BaseComponents/DateTimeBaseComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerSide.BaseComponents
+{
+    public class DateTimeBaseComponent : ComponentBase
+    {
+        public DateTime dateTime { get; set; }
+    }
+}
