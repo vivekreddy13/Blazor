1ee13381e3892390d8e57e6d800a69aa210cf61f
Ponugoti Vivekananda Reddy <vivekreddy2999@gmail.com>
2021-03-01 15:08:44 +0530

    Dependency Injection WebAssembly

---
+++ BlazorWebAssembly/DependencyInjection/DependencyInjection/ICustomerService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+    public interface ICustomerService
+    {
+
+        string Uid { get; set; }
+        Customer GetCustomerById(int id);
+
+    }
+}
